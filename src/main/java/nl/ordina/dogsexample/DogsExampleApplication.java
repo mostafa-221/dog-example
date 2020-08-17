7c64a4bd4b3445175ea9b6d5bc048d3be17ef562
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-08-17 14:50:29 +0200

    printing environment variables
    as a test for azure

--- src/main/java/nl/ordina/dogsexample/DogsExampleApplication.java
+++ src/main/java/nl/ordina/dogsexample/DogsExampleApplication.java
@@ -3,10 +3,16 @@ package nl.ordina.dogsexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class DogsExampleApplication {
 
 	public static void main(String[] args) {
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			System.out.format("%s=%s%n", envName, env.get(envName));
+		}
 		SpringApplication.run(DogsExampleApplication.class, args);
 	}
 
