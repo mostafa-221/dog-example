10e68b7e53bbfc2c7c6fe3c810755288f73e8c8e
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-08-17 15:03:28 +0200

    adding an endpoint to give environment variables
    for azure testing

---
+++ src/main/java/nl/ordina/dogsexample/controller/EnvironmentVariableController.java
@@ -0,0 +1,18 @@
+package nl.ordina.dogsexample.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/EV")
+public class EnvironmentVariableController {
+    @GetMapping
+    public Map<String, String> giveEnvironmentVariables() {
+        return System.getenv();
+    }
+}
