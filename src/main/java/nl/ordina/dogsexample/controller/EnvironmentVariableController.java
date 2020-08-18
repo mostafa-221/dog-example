48106acb92cf98dc024b623556a7a918511ad622
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-08-18 10:35:01 +0200

    returning the endpoint of returning all environment variables

--- src/main/java/nl/ordina/dogsexample/controller/EnvironmentVariableController.java
+++ src/main/java/nl/ordina/dogsexample/controller/EnvironmentVariableController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @CrossOrigin
 @RequestMapping(path = "/EV")
 public class EnvironmentVariableController {
+    @GetMapping
+    public Map<String, String> giveEnvironmentVariables() {
+        return System.getenv();
+    }
 
     @GetMapping(path = "/test")
     public String giveTestEnvironmentVariables() {
