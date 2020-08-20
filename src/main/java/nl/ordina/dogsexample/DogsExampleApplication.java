d16e80017777f614567a8eba18d940902d7d58dd
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-08-20 10:22:31 +0200

    removed the printing of environment variables at the start of the application

--- src/main/java/nl/ordina/dogsexample/DogsExampleApplication.java
+++ src/main/java/nl/ordina/dogsexample/DogsExampleApplication.java
@@ -9,10 +9,6 @@ import java.util.Map;
 public class DogsExampleApplication {
 
 	public static void main(String[] args) {
-		Map<String, String> env = System.getenv();
-		for (String envName : env.keySet()) {
-			System.out.format("%s=%s%n", envName, env.get(envName));
-		}
 		SpringApplication.run(DogsExampleApplication.class, args);
 	}
 
